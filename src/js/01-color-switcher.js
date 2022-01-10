2d6a0a4e04aeb2402e8370189988ac407f6dff94
Alex andr Bodachevskiy <alex.bodachevskiy@gmail.com>
2022-01-10 23:50:26 +0200

    completed 1 task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,35 @@
+const refs = {
+    buttonStart: document.querySelector('.buttonStart'),
+    buttonStop: document.querySelector('.buttonStop'),
+    body: document.querySelector('body')
+}
 
+let timerId = null;
+
+refs.buttonStart.setAttribute('disabled', false);
+refs.buttonStart.disabled = false;
+
+refs.buttonStop.setAttribute('disabled', true);
+refs.buttonStop.disabled = true;
+
+
+function getRandomHexColorForBody() {
+    refs.body.style.backgroundColor = 
+    `#${Math.floor(Math.random() * 16777215).toString(16)}`;     
+};  
+
+function onButtonStartClick() {
+    timerId = setInterval(getRandomHexColorForBody, 1000);
+    refs.buttonStart.disabled = true;
+    refs.buttonStop.disabled = false;
+}
+
+function onButtonStopClick() {
+    clearInterval(timerId);
+    refs.buttonStart.disabled = false;
+    refs.buttonStop.disabled = true;
+}
+
+refs.buttonStart.addEventListener('click', onButtonStartClick);
+
+refs.buttonStop.addEventListener('click', onButtonStopClick);
