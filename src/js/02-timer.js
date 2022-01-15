4a3adfcad1ebb0763a16bad344d7341ec24aa368
Alex andr Bodachevskiy <alex.bodachevskiy@gmail.com>
2022-01-15 20:40:43 +0200

    update 3 task

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -51,13 +51,10 @@ function onButtonStartClick () {
 }
 
 function convertMs(ms) {
-  // Number of milliseconds per unit of time
   const second = 1000;
   const minute = second * 60;
   const hour = minute * 60;
   const day = hour * 24;
-
-  // Remaining days
   const days = Math.floor(ms / day);
   // Remaining hours
   const hours = Math.floor((ms % day) / hour);
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,52 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+let position = null;
+let step = null;
+let amount = null;
+let textForPromise = null;    
+
+const refs = {
+  form: document.querySelector('.form'),
+  position: document.querySelector('input[name=delay]'),
+  step: document.querySelector('input[name=step]'),
+  amount: document.querySelector('input[name=amount]'),
 }
+
+
+refs.form.addEventListener('submit', onButtonClick);
+
+function onButtonClick(e) {
+  e.preventDefault();
+
+  position = Number(refs.position.value);
+  step = Number(refs.step.value);
+  amount = Number(refs.amount.value);  
+  
+  for(let i = 1; i <= amount; i += 1 ) { 
+       textForPromise = position + (step * (i - 1));
+
+    createPromise({ i, textForPromise, step })
+    .then(({ i, textForPromise }) => {
+      Notify.success(`✅ Fulfilled promise ${i} in ${textForPromise} ms`);
+    })
+    .catch(({ i, textForPromise }) => {
+      Notify.failure(`❌ Rejected promise ${i} in ${textForPromise} ms`);
+    });
+  }   
+}
+
+function createPromise(position, amount) {
+  return new Promise((resolve, reject) => {
+   setTimeout(() =>{      
+    const shouldResolve = Math.random() > 0.3;
+    
+    if (shouldResolve) {
+      resolve(position, amount);
+    } else {
+      reject(position, amount);
+      }    
+   }, time (position))
+  }) 
+}
+  
+const time = (position) => position.textForPromise;
